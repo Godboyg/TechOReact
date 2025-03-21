39377329f7e49b374d2ba59e96ee75af6ce8e149
kushal <kushalsinghnegi72@gmail.com>
2025-03-21 08:08:18 +0530

    images saved

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 import NavBar from './Components/NavBar'
 import img from "/img.webp"
 import img2 from "/img2.webp"
+import design from "/Rectangle 1113.png"
+import design1 from "/Rectangle 1112.png"
+import design2 from "/design3.png"
+import design3 from "/design4.png"
+import design4 from "/design5.png"
+import design5 from "/design8.png"
+import design6 from "/design9.png"
+import design7 from "/design10.png"
+import design8 from "/design11.png"
+import design9 from "/design12.png"
 import "./App.css"
 import { Link } from "react-router-dom"
 import { RiArrowDownSLine , RiFacebookLine , RiTwitterLine , RiYoutubeLine , RiInstagramLine , RiShareFill , RiWhatsappLine , RiInformationLine , RiHeartLine , RiSearchLine } from '@remixicon/react'
@@ -11,28 +21,34 @@ function App() {
   const products = [
     {
       id: 1,
-      image: "https://images.pexels.com/photos/31174778/pexels-photo-31174778/free-photo-of-elegant-woman-in-traditional-asian-attire.jpeg?auto=compress&cs=tinysrgb&w=600",
+      image: `${design1}`,
       title: "Buttercup Chanderi Silk & Organza Silk Embro...",
       price: "₹ 1,37,500",
     },
     {
       id: 2,
-      image: "https://images.pexels.com/photos/31193092/pexels-photo-31193092/free-photo-of-young-woman-with-flowers-in-a-park.jpeg?auto=compress&cs=tinysrgb&w=600",
+      image: `${design6}`,
       title: "Buttercup Chanderi Silk & Organza Silk Embro...",
       price: "₹ 1,37,500",
     },
     {
       id: 3,
-      image: "https://images.pexels.com/photos/31157331/pexels-photo-31157331/free-photo-of-elegant-woman-in-red-velvet-traditional-dress.jpeg?auto=compress&cs=tinysrgb&w=600",
+      image: `${design7}`,
       title: "Buttercup Chanderi Silk & Organza Silk Embro...",
       price: "₹ 1,37,500",
     },
     {
       id: 4,
-      image: "https://images.pexels.com/photos/31157371/pexels-photo-31157371/free-photo-of-elegant-woman-in-traditional-vietnamese-dress.jpeg?auto=compress&cs=tinysrgb&w=600",
+      image: `${design8}`,
       title: "Buttercup Chanderi Silk & Organza Silk Embro...",
       price: "₹ 1,37,500",
-    }
+    },
+    {
+      id: 5,
+      image: `${design9}`,
+      title: "Buttercup Chanderi Silk & Organza Silk Embro...",
+      price: "₹ 1,37,500",
+    },
   ]
 
   return (
@@ -54,10 +70,10 @@ function App() {
       </div>
       <div className="flex gap-0.5 max-sm:block">
         <div className="h-[110vh] w-[33vw] max-sm:w-full max-sm:h-[50vh] max-sm:mt-5 overflow-hidden">
-          <img src="https://images.pexels.com/photos/31157323/pexels-photo-31157323/free-photo-of-elegant-woman-in-traditional-velvet-dress.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" className='h-full w-full object-cover'/>
+          <img src={design1} alt="" className='h-full w-full object-cover'/>
         </div>
         <div className="h-[110vh] w-[33vw] max-sm:hidden">
-          <img src="https://images.pexels.com/photos/31174778/pexels-photo-31174778/free-photo-of-elegant-woman-in-traditional-asian-attire.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" className='h-full w-full object-cover'/>
+          <img src={design} alt="" className='h-full w-full object-cover'/>
         </div>
         <div className="py-[4vw] px-[3vw] w-[34vw] max-sm:w-full max-sm:text-xl">
           <div className="">
@@ -122,21 +138,21 @@ function App() {
         <p class="text-center text-[#C69064]"> ✤ ✦ ✧ ✦ ✤</p>
         <div className="mt-12 flex justify-between max-sm:flex max-sm:flex-wrap max-sm:justify-around">
           <div className="w-[39vw] h-[60vh] max-sm:h-[35vh] max-sm:w-[45vw]">
-            <img className='h-full w-full object-cover rounded-md shadow-[8px_8px_15px_rgba(0,0,0,0.3)]' src="https://images.pexels.com/photos/31157323/pexels-photo-31157323/free-photo-of-elegant-woman-in-traditional-velvet-dress.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+            <img className='h-full w-full object-cover rounded-md shadow-[8px_8px_15px_rgba(0,0,0,0.3)]' src={design2} alt="" />
           </div>
           <div className="flex flex-col justify-between max-sm:flex-col max-sm:gap-5">
             <div className="h-[30vh] w-[18vw] max-sm:h-[20vh] max-sm:w-[30vw]">
-              <img className='h-full w-full shadow-[8px_8px_15px_rgba(0,0,0,0.3)] rounded-md object-cover' src="https://images.pexels.com/photos/1057313/pexels-photo-1057313.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+              <img className='h-full w-full shadow-[8px_8px_15px_rgba(0,0,0,0.3)] rounded-md object-cover' src={design3} alt="" />
             </div>
             <div className="h-[27vh] w-[18vw] max-sm:h-[20vh] max-sm:w-[30vw]">
-              <img className='h-full w-full shadow-[8px_8px_15px_rgba(0,0,0,0.3)] rounded-md object-cover' src="https://images.pexels.com/photos/1148960/pexels-photo-1148960.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+              <img className='h-full w-full shadow-[8px_8px_15px_rgba(0,0,0,0.3)] rounded-md object-cover' src={design4} alt="" />
             </div>
           </div>
           <div className="h-[60vh] w-[39vw] max-sm:h-[30vh] max-sm:w-[45vw]">
-            <img className='h-full w-full shadow-[8px_8px_15px_rgba(0,0,0,0.3)] rounded-md object-cover' src="https://images.pexels.com/photos/8386657/pexels-photo-8386657.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+            <img className='h-full w-full shadow-[8px_8px_15px_rgba(0,0,0,0.3)] rounded-md object-cover' src={design5} alt="" />
           </div>
         </div>
-        <div className="text-center mt-30 ">
+        <div className="text-center mt-20">
           <p className='max-sm:text-[3vw]'>Standing tall as the custodians of “zardozi”, Sarhad features archival couture,<br/> handcrafted to perfection. From hundred year old restored “jamavars” to <br/> grandmother’s “suchaa taar” weave.</p>
         </div>
       </div>
@@ -172,19 +188,19 @@ function App() {
           <img className='h-full w-full object-cover' src="https://images.pexels.com/photos/30320422/pexels-photo-30320422/free-photo-of-camel-caravan-crossing-sahara-desert-dunes.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
         </div>
       </div>
-      <div className="h-[125vh] max-sm:h-[100vh]">
+      <div className="h-[125vh] max-sm:h-[95vh]">
         <div className="w-full h-[20vh] mt-5 flex flex-col items-center justify-center">
           <p className='text-[#6E4325]  tracking-tighter mt-[2vw] max-sm:text-[3vw]'>Our recommendations</p>
           <h1 className="text-2xl mt-3 tracking-[0.4vw] font-['EB_Garamond']">EXQUISITE PICKS</h1>
         </div>
         <div className="mt-10 w-full">
-          <div className="whitespace-nowrap scrollbar-hide overflow-x-scroll">
+          <div className="whitespace-nowrap scrollbar-hide overflow-x-scroll ml-10">
           {products.map((product) => (
-            <div key={product.id} className="relative inline-block w-[25vw] max-sm:w-[50vw] ml-5">
+            <div key={product.id} className="relative inline-block w-[20vw] max-sm:w-[50vw] ml-2">
               <img src={product.image} alt={product.title} className="h-96 w-full object-cover" />
               <div className="absolute top-4 right-4">
-                <button className="text-white bg-black/50 rounded-full p-2">
-                  ❤️
+                <button className="text-white bg-black/50 rounded-full p-1">
+                 <span><RiHeartLine size={15}/></span>
                 </button>
               </div>
               <div className="mt-3">
@@ -196,7 +212,7 @@ function App() {
           </div>
         </div>
         <div className="text-center mt-15">
-          <button className="max-sm:text-[3.4vw] border border-white px-6 py-2 text-white hover:bg-white hover:text-black transition">
+          <button className="max-sm:text-[3.4vw] max-sm:mt-10 border border-white px-6 py-2 text-white hover:bg-white hover:text-black transition">
               view all
           </button>
         </div>
@@ -224,7 +240,7 @@ function App() {
            </div>
           </div>
         </div>
-        <div className="h-[40vh] max-sm:h-[60vh] max-sm:w-[85vw] max-sm:text-[4vw]">
+        <div className="h-[40vh] max-sm:h-[60vh] max-sm:mt-5 max-sm:w-[85vw] max-sm:text-[4vw]">
           <h1 className=''>Company</h1>
           <div className="text-[#8B5E3C] mt-5">
             <p className=''>About us</p>
